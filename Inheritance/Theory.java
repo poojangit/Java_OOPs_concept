536adeef3e7ebe24e7cb81734cec1f0e7d879f0f
poojangit <poojang147@gmail.com>
2024-12-29 22:53:31 +0530

    [POOJA] Updated : Advantage of inheritance

---
+++ Inheritance/Theory.java
@@ -0,0 +1,40 @@
+//todo --------------Inheritance----------------
+
+//^ A class achiving a properties of another class is known as inheritance.
+//^ Inheritance is also known as IS-A relationship.
+//^ A class sharing the properties is called Super class/Base class/Parent class
+//^ A class aquiring the properties is called Sub class/derived class/child class
+//^ class inherit the properties of another class using EXTENDS keyword.
+//^ class inherit the properperies of a interface using IMPLEMENTS keyword.
+//^ In java, only variables and methods are inherited . blocks and constrctors are not inherited.
+//^ Java, does not support multiple inheritance with respect to classes.
+//^ Multiple inheritance is possible with respect to interface.
+
+//^ Advantage of inheritance:
+//! code reuasibility
+//! polymorphism
+//! Easy maintainance
+
+//? Types of Inheritance
+//& Single level inheritance
+//& Multi level inheritance
+//& Multiple inheritance
+//& Hierarchical inheritance
+//& Hybrid Inheritance
+
+//& Single level inheritance
+//! It is a combination of one sub class and one super class
+
+//& Multi level inheritance
+//! Combination of multiple sub class and multiple super class
+
+//& Multiple inheritance
+//! combination of one sub class and multiple super class
+
+//& Hierarchical inheritance
+//! combination of multiple subclass with one super class
+
+//& Hybrid inheritance
+//! Combination of multiple inheritance and hierarchical inheritance
+//! Its a combination of two or more type of inheritance.
+
